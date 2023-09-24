8608aa8e187e1921e9bc6fadbf3cdaa83cbf1118
Melissa Olas <melissa.olas@sciam.fr>
2023-09-24 18:12:11 +0200

    adding post, get by id and delete

---
+++ src/books.ts
@@ -0,0 +1,9 @@
+export const books = [
+  {
+    id: 1,
+    title: "Red white and royal blue",
+    author: "Casey McQuiston",
+    summary: "Alex Claremont-Diaz est le fils de la première femme présidente des États-Unis. Alors que sa mère se prépare pour les élections de 2020, Alex provoque un accident avec le prince Henry d'Angleterre lors du mariage du frère de ce dernier. Afin d'éviter que cet accident détourne l'attention de la campagne de réélection de sa mère, les deux garçons qui sont pourtant concurrents depuis toujours, sont obligés de faire semblant d'être amis. Néanmoins, à force de se fréquenter et d'échanger, les deux jeunes hommes vont doucement tomber amoureux, Henry étant gay et Alex réalisant qu'il est bisexuel.",
+    picture: "https://dynamic.indigoimages.ca/v1/books/books/1250332168/1.jpg?width=810&maxHeight=810&quality=85"
+  },
+]
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,47 @@
-import express, { request, response } from "express";
+import express from "express";
 import { books } from "./books";
 
 const server = express();
+server.use(express.json());
 
-server.get("/", (request, response) => {
-    response.send("hello from server");
+server.get("/", (req, res) => {
+    res.send("hello from server");
 });
 
 //GET /books
-server.get("/books", (request, response) => {
-    response.json({ books });
+server.get("/books", (req, res) => {
+    res.json({ books });
 })
 
 //POST /books
-server.post("/books", (request, response) => {
-    //create & return new reading sheet
-    response.send("POST request works");
+server.post("/books", (req, res) => {
+    const book = req.body;
+
+    books.push(book);
+    res.json({ book });
+    res.status(201).json({book})
+})
+
+//GET /book/:id
+server.get("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const book = books.find((book) => book.id === id);
+    if (!book) {
+        res.sendStatus(404);
+    }
+    res.json(book)
+})
+
+//DELETE /books/:id
+server.delete("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const bookIndex = books.findIndex((book) => book.id === id);
+    if (bookIndex === -1) {
+        res.sendStatus(404)
+    }
+    const book = books[bookIndex];
+    books.splice(bookIndex, 1);
+    res.json({ book });
 })
 
 const PORT = 5000;
